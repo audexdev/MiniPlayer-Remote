f571c4ed3e0ca555cbfa04203dcfc902992688d4
audexdev <audex.dev@gmail.com>
2026-01-07 19:08:40 +0900

    Initial Commit

---
+++ MiniPlayerRemote/MiniPlayerRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniPlayerRemoteApp.swift
+//  MiniPlayerRemote
+//
+//  Created by kairi hoshino on 2026/01/07.
+//
+
+import SwiftUI
+
+@main
+struct MiniPlayerRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
